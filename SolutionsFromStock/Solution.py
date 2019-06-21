3ed4c30fed12d28ad387a02f1b3a1cc33743a203
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2019-06-21 07:26:13 -0400

    Update Solution.py

--- SolutionsFromStock/Solution.py
+++ SolutionsFromStock/Solution.py
@@ -1,21 +1,27 @@
 class Solution:
 
     def __init__(self, concentration = 0, compound_formula = ""):
-        self.concentration = concentration
-        self.compound_formula = compound_formula
+        self.__concentration = concentration
+        self.__compound_formula = compound_formula
 
+    #Returns the concentration of the solution
     def get_concentration (self):
-        return(self.concentration)
+        return(self.__concentration)
 
+    #Returns the chemicalforumla of the solution
     def get_compound_formula(self):
-        return(self.compound_formula)
+        return(self.__compound_formula)
 
+    #Changes the concentration of the solution to new_conc
     def set_concentration (self, new_conc):
-        self.concentration = new_conc
+        self.__concentration = new_conc
 
+    #Changes the chemicalformula of the solution to new_formula
+    def set_compound_formula(self, new_formula):
+        self.__compound_formula = new_formula
 
     def __str__ (self):
-        return("Solution: %s mM %s" % (self.get_concentration(), self.get_compound_formula()))
+        return("%s mM %s" % (self.get_concentration(), self.get_compound_formula()))
 
     if (__name__ == "__main__"):
         print("This is the class file for the object 'Solution object'")
