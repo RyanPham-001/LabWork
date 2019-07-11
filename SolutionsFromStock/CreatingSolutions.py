691e8ef8f7fc2c7aae5e3774b9d73b8263a4cdc2
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2019-07-10 21:20:15 -0400

    Update CreatingSolutions.py

--- SolutionsFromStock/CreatingSolutions.py
+++ SolutionsFromStock/CreatingSolutions.py
@@ -1,9 +1,34 @@
 from Solution import Solution
+from SolutionList import SolutionList
 
-test = Solution(400,"NaCl")
-print(test.get_concentration())
-print(test)
+class CreatingSolutions:
 
-test.set_compound_formula("KCl")
-test.set_concentration(300)
-print(test)
+    def __init__(self,final_volume):
+        self.___final_volume = 0
+        self.___check_first = False
+        self.___check_last = False
+        self.___StockSolutionList = SolutionList()
+        #how to keep the composition togeter? use dictionary or tuple?
+        
+    def getFinalVolume (self):
+        return self.___final_volume
+
+    def makeSolution(self,file_name):
+        pass
+
+    def C1V1equalsC2V2(self, final_volume):
+        pass
+
+
+
+def main():
+    test = Solution(400,"NaCl")
+    print(test.get_concentration())
+    print(test)
+
+    test.set_compound_formula("KCl")
+    test.set_concentration(300)
+    print(test)
+
+if (__name__ == '__main__'):
+    main()
