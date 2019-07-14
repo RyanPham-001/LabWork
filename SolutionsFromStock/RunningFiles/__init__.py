2efbec37afa24673a78ab3b3e40995b3bbc97f39
Ryan Pham <hpluckyryan99@gmail.com>
2019-07-14 19:02:18 -0400

    testing packages

---
+++ SolutionsFromStock/RunningFiles/__init__.py
@@ -0,0 +1,4 @@
+from CreatingSolutions import CreatingSolutions
+from NoneError import NoneError
+from Solution import Solution
+from StockSolutionList import StockSolutionList
--- SolutionsFromStock/TestFiles/test_StockSolutionList.py
+++ SolutionsFromStock/TestFiles/test_StockSolutionList.py
@@ -1,33 +1,50 @@
 import unittest
-from SolutionsFromStock.StockSolutionList import StockSolutionList
-from CreatingSolutions import CreatingSolutions
-from Solution import Solution
-from NoneError import NoneError
+import sys
+import os
 
-#Find out a way to import multiple files without all of these lines
 
-class TestStockSolution(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+from pathlib import Path
+y = Path(__file__).resolve().parent
+print (y)
+sys.path.append('../')
+print(sys.path)
+x = Path(__file__).resolve().parent.parent
+print(x)
+sys.path.insert(0,x)
 
-    def setUp(self):
-        self.testList = StockSolutionList()
+os.chdir(x)
+print(sys.path)
+os.chdir(y)
 
-    def tearDown(self):
-        pass
-
-    def test_add_single_solution(self):
-        sol = Solution(450, "NaCl")
-
-        self.assertTrue(self.testList.isEmpty(),"List should be empty initially")
-
-        self.testList.add(sol)
-        self.assertFalse(self.testList.isEmpty(),"List was expected to not be empty")
-        self.assertEqual(self.testList.getSize(),1,"List expected to have a size of 1")
-
-    def test_empty_contains(self):
-        with self.assertRaises(NoneError):
-            self.testList.contains("KCl")
-
-if (__name__ == "__main__"):
-    unittest.main()
+# from StockSolutionList import StockSolutionList
+# from CreatingSolutions import CreatingSolutions
+# from Solution import Solution
+# from NoneError import NoneError
+#
+# #Find out a way to import multiple files without all of these lines
+#
+# class TestStockSolution(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         pass
+#     def setUp(self):
+#         self.testList = StockSolutionList()
+#
+#     def tearDown(self):
+#         pass
+#
+#     def test_add_single_solution(self):
+#         sol = Solution(450, "NaCl")
+#
+#         self.assertTrue(self.testList.isEmpty(),"List should be empty initially")
+#
+#         self.testList.add(sol)
+#         self.assertFalse(self.testList.isEmpty(),"List was expected to not be empty")
+#         self.assertEqual(self.testList.getSize(),1,"List expected to have a size of 1")
+#
+#     def test_empty_contains(self):
+#         with self.assertRaises(NoneError):
+#             self.testList.contains("KCl")
+#
+# if (__name__ == "__main__"):
+#     unittest.main()
